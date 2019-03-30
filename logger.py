c03da705fe6f88f6835854210b23a683aa6324af
Li Yin <liyin@scripps.edu>
2019-03-29 19:33:32 -0700

    Moved logger and query_cache to utils folder

--- logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-import os
-
-LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', logging.INFO)
-
-# configure logger
-logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', level=LOGGING_LEVEL)
-logger = logging.getLogger('MetaMaPY')
-logger.info(f'LOGGING_LEVEL={LOGGING_LEVEL}')
