c108571c0d6b80581294aadbb5881e0da7f7dcf6
Li Yin <liyin@scripps.edu>
2019-03-29 19:34:49 -0700

    Updated due to import changes. Added Keyword resource.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-import settings  # load .env files first
-from logger import logger
+from dotenv import load_dotenv
+from pathlib import Path
+from utils.logger import logger
 from flask import Flask
 from flask_restful import Api
-from resources.variant import Variant
+from resources.terms import Article, Keyword
+
+# explicitly providing path to '.env'
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path, verbose=True)
 
 app = Flask(__name__)
 api = Api(app)
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # limit request size to 10MB
 app.config['PROPAGATE_EXCEPTIONS'] = True
 logger.info('app starts.')
 
@@ -17,7 +23,8 @@ def after_request(response):
     return response
 
 
-api.add_resource(Variant, '/metamap/rsid/<string:rsid>')
+api.add_resource(Article, '/metamap/articles')
+api.add_resource(Keyword, '/metamap/keyword/<string:keyword>')
 
 if __name__ == '__main__':
     app.run(debug=True)
